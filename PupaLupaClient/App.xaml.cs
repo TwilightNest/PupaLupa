f6f5e5589e00a646d34af5fb4f2ea700cc3ccaea
TwilightNest <Jarik_Voinski@mail.ru>
2022-12-15 01:31:34 +0300

    create default projects

---
+++ PupaLupaClient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PupaLupaClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
